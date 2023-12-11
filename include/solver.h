2bb6435db38390e4a2190b972ea5804127d01e0c
Khamikova-Marina <153535824+Khamikova-Marina@users.noreply.github.com>
2023-12-11 21:28:23 +0300

    Update and rename solver.h to include/solver.h

---
+++ include/solver.h
@@ -0,0 +1,6 @@
+#pragma  once
+
+#include "vector.h" 
+#include "matrix.h"
+
+Vector solver(Matrix A, Vector b, double alpha, int number_of_iterations);
--- solver.h
+++
@@ -1,6 +0,0 @@
-#pragma  once
-
-#include "vector.h" 
-#include "matrix.h"
-
-Vector solver(Matrix A, Vector b, double alpha, int number_of_iterations);
